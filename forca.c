5658cc0ed4a3ec16279d5d92e7921239fb77015d
unknown <cravo.diego@gmail.com>
2019-10-22 02:21:04 -0300

    Fim da aula 4

--- forca.c
+++ forca.c
@@ -2,47 +2,69 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+	char palavrasecreta[20];
+	char chutes [26];
+	int tentativas = 0;
+
+void abertura() {
+	printf("**/****************/\n");
+	printf("*/***Jogo Forca***/*\n");
+	printf("/****************/**\n\n");
+}
+
+void chuta () {
+	char chute;
+	printf("Qual a letra? ");
+	scanf(" %c", &chute);
 	
-	char palavrasecreta [20];
+    chutes[tentativas] = chute;
+}
+
+int jachutou(char letra){
+				int achou = 0;
+				for (int j = 0; j < tentativas; j++){
+					if(chutes [j] == letra){
+						achou = 1;
+						break;
+					}
+			}
+			return achou;
+}
+
+void desenhaforca (){
+			
+			printf("Voce ja deu %d chutes\n", tentativas);
+			
+			for (int i = 0; i < strlen(palavrasecreta); i++){
+			
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }	
+    printf("_ \n");
+}
+
+void escolhepalavra (){
 	sprintf(palavrasecreta, "CROSSFIT");
+}
+
+int main() {
 
 	int acertou = 0;
 	int enforcou = 0;
 	
-	char chutes [26];
-	int tentativas = 0;
+	abertura ();
+	escolhepalavra();	
 	
 	do{
+		desenhaforca();
+		chuta ();	
 		
-		for (int i = 0; i < strlen(palavrasecreta); i++){
-			
-			int achou = 0;
-			
-			for (int j = 0; j < tentativas; j++){
-				if(chutes [j] == palavrasecreta[i]){
-					achou = 1;
-					break;
-				}
-			}
-			
-			if(achou) {
-				printf("%c ", palavrasecreta[i]);
-			} else {
-				printf("_ ");
-			}			
-		}
-		printf("\n");
-		
-		char chute;
-		printf("Qual a letra? ");
-		scanf(" %c", &chute);
-		
-        chutes[tentativas] = chute;
-        tentativas++;		
+		tentativas++;	
 				
 	} while (!acertou && !enforcou);
-	
-		
 		
 }
